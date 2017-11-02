4ceecddce5a9c7efa46d0e0f442da033ade67f52
Michael Buckley <michael@ntree.com>
2017-11-02 15:42:22 -0400

    exclude workspace

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 . ./install.sh
 python backup.py --bucket=ntree-infrastructure-backup --bucket-region=us-east-1 \
-       create --include-archive --include-builds --include-logs
+       create --include-archive --include-builds --include-logs --exclude-workspace
