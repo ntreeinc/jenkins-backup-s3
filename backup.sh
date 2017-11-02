0a0abbb69ff19ffd1bf50af483b408c3a1efc4f3
Michael Buckley <michael@ntree.com>
2017-11-02 15:46:02 -0400

    [#138457169] Add prune to default backup job

--- backup.sh
+++ backup.sh
@@ -1,3 +1,6 @@
 . ./install.sh
 python backup.py --bucket=ntree-infrastructure-backup --bucket-region=us-east-1 \
        create --include-archive --include-builds --include-logs --exclude-workspace
+python backup.py --bucket=ntree-infrastructure-backup --bucket-region=us-east-1 \
+       prune
+
