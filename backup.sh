de83dd8c5352bec7ad58cfefdabb6b5f8dbcc4ac
Michael Buckley <michael@ntree.com>
2017-11-02 15:40:37 -0400

    [#138457169] fix backup

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 . ./install.sh
-python backup.py --bucket=ntree-infrastructure-backup --bucket-region=us-east-1 backup \
-       --include-archive --include-builds --include-logs
+python backup.py --bucket=ntree-infrastructure-backup --bucket-region=us-east-1 \
+       create --include-archive --include-builds --include-logs
