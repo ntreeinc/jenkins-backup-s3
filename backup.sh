34d213d09d02b777f51ffd1f0a6bbc04465268ab
Michael Buckley <michael@ntree.com>
2018-05-29 15:42:04 -0400

    Move to Canada

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 . ./install.sh
-python backup.py --bucket=ntree-infrastructure-backup --bucket-region=us-east-1 \
+python backup.py --bucket=ntree-infrastructure-backup-ca --bucket-region=ca-central-1 \
        create --include-archive --include-builds --include-logs --include-workspace
-python backup.py --bucket=ntree-infrastructure-backup --bucket-region=us-east-1 \
+python backup.py --bucket=ntree-infrastructure-backup-ca --bucket-region=ca-central-1 \
        prune 7
 
