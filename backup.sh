cacaa3debf80023860e4054fda5ea8adf975dc22
Michael Buckley <michael@ntree.com>
2018-05-29 15:34:58 -0400

    Add workspaces

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 . ./install.sh
 python backup.py --bucket=ntree-infrastructure-backup --bucket-region=us-east-1 \
-       create --include-archive --include-builds --include-logs --exclude-workspace
+       create --include-archive --include-builds --include-logs --include-workspace
 python backup.py --bucket=ntree-infrastructure-backup --bucket-region=us-east-1 \
        prune 7
 
