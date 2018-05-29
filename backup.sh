72b51e095571de5a2e7d8f3d7db98d41f434738b
Michael Buckley <michael@ntree.com>
2018-05-29 16:18:45 -0400

    Add workspaces back in

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 . ./install.sh
 . ./config.sh
 python backup.py --bucket=${backup_bucket_name} --bucket-region=${backup_bucket_region} \
-       create --include-archive --include-builds --include-logs --exclude-workspace
+       create --include-archive --include-builds --include-logs --include-workspace
 python backup.py --bucket=${backup_bucket_name} --bucket-region=${backup_bucket_region} \
        prune 7
 
