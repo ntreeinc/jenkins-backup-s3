f0cebbc3e9dcf339bb4b661eb42e7ee48d4fd1e7
Michael Buckley <michael@ntree.com>
2018-05-29 15:58:17 -0400

    exclude workspace for testing

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 . ./install.sh
 python backup.py --bucket=ntree-infrastructure-backup-ca --bucket-region=ca-central-1 \
-       create --include-archive --include-builds --include-logs --include-workspace
+       create --include-archive --include-builds --include-logs --exclude-workspace
 python backup.py --bucket=ntree-infrastructure-backup-ca --bucket-region=ca-central-1 \
        prune 7
 
