5883620884831836fed4aaadf0dad5a0712fda87
Michael Buckley <michael@ntree.com>
2018-06-05 10:32:48 -0400

    Try to include more

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 . ./install.sh
 . ./config.sh
 python backup.py --bucket=${backup_bucket_name} --bucket-region=${backup_bucket_region} \
-       create --include-archive --include-builds --include-logs --include-workspace
+       create --include-archive --include-target --include-builds --include-workspace --include-maven --include-logs
 python backup.py --bucket=${backup_bucket_name} --bucket-region=${backup_bucket_region} \
        prune 7
 
