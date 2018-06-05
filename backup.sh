28da54041b521447adbe735e6632897bccb6e7db
Michael Buckley <michael@ntree.com>
2018-06-05 10:47:16 -0400

    Ignore our working yum copies

--- backup.sh
+++ backup.sh
@@ -1,7 +1,8 @@
 . ./install.sh
 . ./config.sh
 python backup.py --bucket=${backup_bucket_name} --bucket-region=${backup_bucket_region} \
-       create --include-archive --include-target --include-builds --include-workspace --include-maven --include-logs
+       create --include-vcs --include-archive --include-target --include-builds --include-workspace \
+         --include-maven --include-logs --exclude 'workspace/*/build/yum'
 python backup.py --bucket=${backup_bucket_name} --bucket-region=${backup_bucket_region} \
        prune 7
 
