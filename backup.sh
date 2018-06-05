73834d45235747eee92620818bac7d34d28dd773
Michael Buckley <michael@ntree.com>
2018-06-05 17:31:26 -0400

    Exclude backup job logfile from backup

--- backup.sh
+++ backup.sh
@@ -1,8 +1,9 @@
 . ./install.sh
 . ./config.sh
+# the job logfile was confusing tar
 python backup.py --bucket=${backup_bucket_name} --bucket-region=${backup_bucket_region} \
        create --include-vcs --include-archive --include-target --include-builds --include-workspace \
-         --include-maven --include-logs --exclude 'workspace/*/build/yum'
+         --include-maven --include-logs --exclude 'workspace/*/build/yum' --exclude 'jobs/backups/jobs/jenkins-backup/builds/*/log' || exit 1
 python backup.py --bucket=${backup_bucket_name} --bucket-region=${backup_bucket_region} \
        prune 7
 
